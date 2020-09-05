d95b42af4620e5690e79a7a4fb5fa039d55f6604
Varun Kumar <32612311+vs1997@users.noreply.github.com>
2020-09-05 16:25:37 +0530

    Update Body.jsx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,13 +5,15 @@ function About() {
     <div>
       <h1 className='about'>About Me</h1>
       <p className='bio'>
-        Saturday Developer builds simple, powerful products for people and
-        businesses. A full stack web development is providing by me who can
-        develop both client and server software. In addition to mastering HTML
-        and CSS, i can knows how to Program a browser (like using JavaScript,
-        jQuery, React js, React Native),Program a server (like using Node Js,
-        Python, Firebase.com,Heroku),Program a database (like using SQL,
-        MongoDB, NoSQL).
+       I builds simple, powerful product for people
+and business. I used to work as full stack developer for web and mobile as well, Also I am open to work for front-end only or back-end  only as per needed.
+
+I have quite good experience in Node Js, Mongo DB, React Js, React-Native, Redux, Context-API,Express, Firebase, HTML, CSS, Javascript. 
+ 
+Also I have good knowledge for versioning tools e.g:- Git, SVN, BitBUcket.
+
+
+Apart of this, I used to post videos on YouTube whenever I have free time.  You can find my YouTube Channel link from the footer section.
       </p>
       <hr />
     </div>
